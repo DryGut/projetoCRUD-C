3890806d7a21fbb59f9191061e3da09b03279d6a
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-08-04 18:43:58 +0000

    crud lista encadeada atualizado

--- crudLinkedList.c
+++ crudLinkedList.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 //constante para definir o tamanho das strings
 #define TAM 50
+
 //estrutura dos dados
 typedef struct {
 int id;
@@ -12,21 +13,26 @@ char name[TAM];
 char address[TAM];
 char phone[TAM];
 }cadastro;
+
 //no para manipular a lista
 struct No{
 cadastro cad;                //instancia a struct para ser inserida
 struct No* prox;            //struct que apontará para o proximo nó
 };
+
 //lista propriamente dita
 struct Lista{
 struct No* inicio;         //ponteiro para o primeiro nó da lista
 int tamanho;              //tamanho da lista
 };
+
 //escopo das funções
 struct Lista* criar();
-void inserir(struct Lista*, int pos, cadastro cad);
-bool vazia();
-void imprimir();
+void inserir(struct Lista* li, int pos, cadastro cad);
+bool vazia(struct Lista* li);
+void imprimir(struct Lista* li);
+void buscar(struct Lista* li, int pos);
+
 //função que criará a lista
 struct Lista* criar(){
 
@@ -38,6 +44,7 @@ struct Lista* criar(){
   }
   return nova_lista;
 }
+
 //função para verificar se a lista esta vazia
 bool vazia(struct Lista* li){
   
@@ -48,19 +55,21 @@ bool vazia(struct Lista* li){
     return false;
   }
 }
+
 //função para inserir os dados na lista
 void inserir(struct Lista* li, int pos, cadastro cad){
 
   assert(li != NULL);//verifica se o ponteiro não é nulo
   assert(pos >= 0 && pos <= li->tamanho);//verifica a posição de inserção
+  
   //instancia um novo nó para armazenar um novo elemento
   struct No* novo_no = (struct No*) malloc(sizeof(struct No));
-  novo_no->cad = cad;//atualizando a variavel do novo nó
+  novo_no->cad = cad;         //atualizando a variavel do novo nó
 
-  if(pos == 0){//verifica se a inserção do elemento é no inicio da lista
+  if(pos == 0){              //verifica se a inserção do elemento é no inicio da lista
     novo_no->prox = li->inicio;
     li->inicio = novo_no;
-  }else{//insere o elemento numa posição qualquer da lista
+  }else{                     //insere o elemento numa posição qualquer da lista
     struct No* aux = li->inicio;
     for(int i=0; i<pos-1; i++){
       aux = aux->prox;
@@ -70,6 +79,23 @@ void inserir(struct Lista* li, int pos, cadastro cad){
   }
   li->tamanho++;
 }
+
+//função para obter o elemento da lista conforme o indice passado
+void buscar(struct Lista* li, int pos){
+  
+  assert(li != NULL);
+  assert(pos >= 0 && pos < li->tamanho);
+  
+  struct No* aux = li->inicio;
+  for(int i=0; i<pos; i++){
+    aux = aux->prox;
+  }
+  printf("\nId: %d", aux->cad.id);
+  printf("\nNome: %s", aux->cad.name);
+  printf("Endereço: %s", aux->cad.address);
+  printf("Telefone: %s", aux->cad.phone);
+}
+
 //função que irá gerar os dados da estrutura
 cadastro montar_cad(){
  
@@ -96,6 +122,7 @@ cadastro montar_cad(){
 
   return *cad;
 }
+
 //função que irá imprimir os dados armazenados na lista
 void imprimir(struct Lista* li){
 
@@ -110,11 +137,13 @@ void imprimir(struct Lista* li){
     aux = aux->prox;
   }
 }
+
 //programa principal
 int main(){
   struct Lista* registros = criar();
   cadastro cad;
   int opcao;
+  int reg;
   do{
     system("echo \033[92m");//define a cor do menu
     printf("\n=================================");
@@ -122,8 +151,8 @@ int main(){
     printf("\n=================================");
     printf("\n|   Selecione a Opção Desejada  |");
     printf("\n|   [1] - Para Inclusão         |");
-    printf("\n|   [2] - Para Imprimir         |");
-    printf("\n|   [3] - Para Sair             |");
+    printf("\n|   [2] - Para Imprimir Todos   |");
+    printf("\n|   [3] - Para Imprimir Esp     |");
     printf("\n=================================");
     printf("\nDigite a Opção Desejada: ");
     scanf("%d", &opcao);
@@ -137,7 +166,11 @@ int main(){
       case 2:
         system("clear");
         imprimir(registros);
+        break;
       case 3:
+        printf("Digite o Registro: ");
+        scanf("%d", &reg);
+        obter(registros, reg);
         break;
       }
     }while(opcao != 3);
